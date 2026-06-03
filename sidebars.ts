a60c2c84c0efa30cfa6ad232fb77073ca8217cf4
Ayush8923 <80516839+Ayush8923@users.noreply.github.com>
2026-06-03 19:30:27 +0530

    fix(docs): remove the platform overview.

--- sidebars.ts
+++ sidebars.ts
@@ -4,7 +4,6 @@ const sidebars: SidebarsConfig = {
   tutorialSidebar: [
     {type: 'doc', id: 'docs', label: 'Kaapi Overview'},
     {type: 'doc', id: 'architecture', label: 'How Kaapi Works'},
-    {type: 'doc', id: 'platform-overview', label: 'Platform Overview'},
     {type: 'doc', id: 'core-use-cases', label: 'Core Use Cases'},
     {type: 'doc', id: 'getting-started', label: 'Getting Started'},
   ],
